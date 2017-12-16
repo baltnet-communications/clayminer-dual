ab00549d637cb5808f813a9068ce52fdac255bcb
The Ranger <ranger@risk.ee>
2017-12-16 20:22:42 +0200

    Initial commit

---
+++ debian/clayminer-dual.default.ex
@@ -0,0 +1,10 @@
+# Defaults for ethminer initscript
+# sourced by /etc/init.d/ethminer
+# installed at /etc/default/ethminer by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
